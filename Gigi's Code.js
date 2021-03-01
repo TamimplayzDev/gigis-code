6fad2c888ff4823706b357e523e2e32ef5d5846f
TamimplayzDev <74967194+TamimplayzDev@users.noreply.github.com>
2021-03-01 16:17:28 +0600

    Add files via upload

---
+++ Gigi's Code.js
@@ -0,0 +1,12 @@
+var color = 0
+print (cry); 
+trace (sorrow);
+if (gigi - you);
+type (end);
+// What's a game with no pleasure?
+function loop(
+    untilGigiAndGame = together
+)
+if (home = whatWeEarn);
+goto (me)
+press (RETURN)
